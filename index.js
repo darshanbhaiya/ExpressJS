6180b7924c5d97517a0fb858e6fb27b8247908ef
Darshan Bhaiya <darshanbhaiya0403@gmail.com>
2024-05-13 23:14:02 +0530

    first day of learning

---
+++ index.js
@@ -0,0 +1,33 @@
+// basic unopiniated framework for node.js
+// Node.js => browser pe run hone wali java script ko server pe run karta hai
+        //    => for chrome it runs through v8 engine
+        //    => npm is package manager for node.js => get installed into node modules
+        //    => we can use extra packages through npm
+        // => we use express in place of node to for easy application on end points
+
+
+// 500 server code for internal error
+// 400 server code for =>  server can't process or recognize the request.
+
+const express = require('express')
+const path = require('path')  // we imported a module path
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+// res.send('aboutt!')    
+res.sendFile(path.join(__dirname,'index.html'))
+res.status(400)
+})
+
+app.get('/aboutt', (req, res) => {
+res.json({"harry":34,"darshan":34})
+})
+
+app.listen(port, () => {
+console.log(`Example app listening on port at http://localhost:${port}`)
+})        
